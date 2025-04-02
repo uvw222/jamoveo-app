022f3b368ce60f82cbc7d6ae914392e40bcfe3a0
uvw222 <yuval2wil@gmail.com>
2025-04-02 21:07:09 +0300

    changing the signup component

--- jamoveo-app/src/components/Signup.js
+++ jamoveo-app/src/components/Signup.js
@@ -1,5 +1,5 @@
-// src/components/Signup.js
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Signup() {
   const [formData, setFormData] = useState({
@@ -14,31 +14,49 @@ function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you would typically send the data to your server.
+    // TODO: Add your signup logic (API call, etc.)
     console.log('Signup form submitted:', formData);
-    // Redirect to login page or main page after signup.
   };
 
   return (
-    <div>
-      <h2>Signup Page</h2>
+    <div style={{ margin: '2em', textAlign: 'center' }}>
+      <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-        <br />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-        <br />
-        <select name="instrument" onChange={handleChange} required>
-          <option value="">Select Instrument</option>
-          <option value="guitar">Guitar</option>
-          <option value="drums">Drums</option>
-          <option value="bass">Bass</option>
-          <option value="saxophone">Saxophone</option>
-          <option value="keyboards">Keyboards</option>
-          <option value="vocals">Vocals</option>
-        </select>
-        <br />
-        <button type="submit">Signup</button>
+        <div>
+          <input 
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <select name="instrument" onChange={handleChange} required>
+            <option value="">Select Instrument</option>
+            <option value="guitar">Guitar</option>
+            <option value="drums">Drums</option>
+            <option value="bass">Bass</option>
+            <option value="saxophone">Saxophone</option>
+            <option value="keyboards">Keyboards</option>
+            <option value="vocals">Vocals</option>
+          </select>
+        </div>
+        <button type="submit">Sign Up</button>
       </form>
+      <p>
+        Already registered?{' '}
+        <Link to="/login">Click here to log in</Link>.
+      </p>
     </div>
   );
 }
