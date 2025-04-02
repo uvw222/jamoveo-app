b5ea0813e904a0ea573b203b7ed15bff7c20d62c
uvw222 <yuval2wil@gmail.com>
2025-04-02 21:40:00 +0300

    fixing choosing song 2

--- jamoveo-app/src/components/PlayerMain.js
+++ jamoveo-app/src/components/PlayerMain.js
@@ -7,21 +7,23 @@ function PlayerMain() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('PlayerMain: Connecting to socket...');
+    console.log('PlayerMain: mounting and connecting...');
+    
     socket.on('connect', () => {
       console.log('PlayerMain: Connected to socket with id', socket.id);
     });
-
+  
     socket.on('songUpdate', (songData) => {
       console.log('PlayerMain: Received songUpdate:', songData);
-      // Navigate to Live page when a song update is received
       navigate('/live', { state: { song: songData, userRole: 'player' } });
     });
-
+  
     return () => {
+      console.log('PlayerMain: unmounting, removing songUpdate listener');
       socket.off('songUpdate');
     };
   }, [navigate]);
+  
 
   return (
     <div style={{ textAlign: 'center', marginTop: '2em' }}>
