7d91a28ca7b5a3c834fae14aaa9ba0b869bd9567
uvw222 <yuval2wil@gmail.com>
2025-04-02 21:08:01 +0300

    changing the login component

--- jamoveo-app/src/components/Login.js
+++ jamoveo-app/src/components/Login.js
@@ -1,6 +1,5 @@
-// src/components/Login.js
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
   const [credentials, setCredentials] = useState({ username: '', password: '' });
@@ -12,25 +11,43 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you would validate credentials with your server.
+    // TODO: Add your login logic (API call, authentication, etc.)
     console.log('Login submitted:', credentials);
-
-    // For demo, navigate to the player page.
+    // For now, let's navigate to a placeholder page
     navigate('/player');
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div style={{ margin: '2em', textAlign: 'center' }}>
+      <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-        <br />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-        <br />
-        <button type="submit">Login</button>
+        <div>
+          <input 
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Log In</button>
       </form>
+      <p>
+        Don't have an account?{' '}
+        <Link to="/signup">Sign up here</Link>.
+      </p>
     </div>
   );
 }
 
 export default Login;
+
