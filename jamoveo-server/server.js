bd483b6cacbd3600cd06305f37de15fada706e97
uvw222 <yuval2wil@gmail.com>
2025-04-02 19:37:07 +0300

    committing server for deployment

--- jamoveo-server/server.js
+++ jamoveo-server/server.js
@@ -4,16 +4,12 @@ const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
-
-// Create HTTP server and attach Socket.IO
 const server = http.createServer(app);
 const io = socketIo(server, {
-  cors: {
-    origin: '*'
-  }
+  cors: { origin: '*' } // Adjust CORS settings as needed
 });
 
-// Basic route for testing
+// A simple route for testing
 app.get('/', (req, res) => {
   res.send('JaMoveo Server is running.');
 });
@@ -22,13 +18,13 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
 
-  // When the admin selects a song, broadcast it to all clients
+  // Example: when admin selects a song, broadcast it
   socket.on('selectSong', (songData) => {
     console.log('Song selected:', songData);
     io.emit('songUpdate', songData);
   });
 
-  // Handle quit session command
+  // When the admin quits the session
   socket.on('quitSession', () => {
     console.log('Session quit by admin.');
     io.emit('sessionQuit', {});
